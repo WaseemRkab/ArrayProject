df32259067fb0eb18461ea3fc333686e0755303e
Waseem Rkab <rakab.waseem@gmail.com>
2020-03-07 14:32:46 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -224,3 +224,15 @@ export class AppComponent implements OnInit {
     this.clipBoardNotCopied.fire();
   }
 }
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker('./app.worker', {type: 'module'});
+  worker.onmessage = ({data}) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
